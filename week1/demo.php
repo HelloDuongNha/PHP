9e018e16ecca7c52094e108655e8292d1b40cf3a
HelloDuongNha <hoangzuong9a1306@gmail.com>
2024-10-04 20:14:25 +0700

    init project

--- week1/demo.php
+++ week1/demo.php
@@ -10,17 +10,7 @@ echo "<h1 style='color:darkgreen';>hello \"world\" <br><h1>";
 print "<h1>we are studying PHP<h1>";
 
 
-$a = -5;
-if ($a < 0):
-?>
-    <ul>
-        <li>hello</li>
-        <li>world</li>
-    </ul>
-<?php
-else:
-    echo "a is positive";
-endif;
+
 
 
 ?>
